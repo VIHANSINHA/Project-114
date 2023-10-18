bd64d8771af0996746ea1a4dad299f8fcb606718
VIHANSINHA <108222959+VIHANSINHA@users.noreply.github.com>
2023-10-18 19:29:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){
+
+}
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.position(400,180);
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+}
+function draw(){
+
+}
